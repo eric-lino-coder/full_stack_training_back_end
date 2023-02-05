28ffd10bb359a5c3d0aa5eaf80b11d3c90dcc32b
eric-lino-coder <ericwillian2018@gmail.com>
2023-02-05 19:46:14 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, make_response, jsonify
+from bd import Carros
+
+app = Flask(__name__)
+
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    return  make_response(
+        jsonify(Carros)
+    )
+
+app.run()
+
