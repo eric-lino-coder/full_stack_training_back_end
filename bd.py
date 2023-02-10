caaf252add41308807d28292afd798cd70adc2aa
eric-lino-coder <ericwillian2018@gmail.com>
2023-02-10 18:12:47 -0300

    feat: fake db

--- bd.py
+++ bd.py
@@ -1,1 +1,45 @@
-Carros = 'eric'
+pessoas = [
+    {
+        "id":1,
+        "nome": "talles",
+        "idade": 24,
+        "cpf":"45668483886",
+        "instituição":"NU PAGAMENTOS - IP",
+    },
+    {
+        "id": 2,
+        "nome": "eric",
+        "idade": 18,
+        "cpf":"45668527840",
+        "instituição": "NU PAGAMENTOS - IP",
+    },
+    {
+        "id": 3,
+        "nome": "hendricck",
+        "idade": 22,
+        "cpf":"456621212",
+        "instituição": "BANCO DO BRASIL",
+    },
+    {
+        "idade": 19,
+        "nome": "felipe",
+        "cpf":"18796543268",
+        "instatuição":"PICPAY - PAGAMENTOS",
+        "id": 4,
+    },
+    {
+        "id": 5,
+        "nome": "osvaldo",
+        "idade": 36,
+        "cpf":"12546978632",
+        "instatuição":"BANCO DO BRASIL",
+        
+    }, 
+    {
+        "id": 6,
+        "nome": "pedro",
+        "idade": 24,
+        "cpf":"45023654784",
+        "instatuição":"NU PAGAMENTOS - IP",
+    },
+]
