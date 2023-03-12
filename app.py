139942bf735f98360783f95c393d42e2cbde8722
eric-lino-coder <ericwillian2018@gmail.com>
2023-03-11 22:53:45 -0300

    feat: added function to delete database users

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(hos
 conn = psycopg2.connect(conn_string)
 print("Connection established")
 
-cursor = conn.cursor()
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -24,11 +23,12 @@ app.config['JSON_SORT_KEYS'] = False
 @app.route('/carros', methods=['GET'])
 def get_carros():
 
-    # Drop previous table of same name if one exists
-
+    #cusor.execute é o comando que roda uma query no banco de dados configurado pré-viamente 
+    cursor = conn.cursor()
     cursor.execute("select usr_id, usr_name from tb_users;")
-    dadosDoBanco = cursor.fetchall()
-    print("Finished dropping table (if existed)")
+    dadosDoBanco = cursor.fetchall() 
+    cursor.close()
+
     return  make_response(
         jsonify(dadosDoBanco)
     )
@@ -36,24 +36,42 @@ def get_carros():
 
 @app.route('/carros', methods=['POST'])
 def create_carro():
-    dadosRecebidosDoFrontEnd = request.json ("pessoas")
+    #request.json é a variavel que esta armazenando os dados que o front-end esta enviando
+    cursor = conn.cursor()
+    dadosRecebidosDoFrontEnd = request.json
+
+    #aqui estamos acessando aspropriedades do objeto recebido do front-end
+    nomeRecebidosDoFrontEnd = dadosRecebidosDoFrontEnd["usr_name"]
+    pagamentoRecebidoDoFrontEnd = dadosRecebidosDoFrontEnd["usr_dadospagamento"]
     
-    nomeRecebidosDoFrontEnd = dadosRecebidosDoFrontEnd["pessoas"]
+    print("nomeRecebidosDoFrontEnd:",nomeRecebidosDoFrontEnd)
+    print("pagamentoRecebidoDoFrontEnd:",pagamentoRecebidoDoFrontEnd)
+
+    query = "INSERT INTO tb_users (usr_name , usr_dadospagamento) values ('{p2}', '{p3}');"
+    formatted_query = query.format(p2=nomeRecebidosDoFrontEnd, p3=pagamentoRecebidoDoFrontEnd)
+    cursor.execute(formatted_query)
+    conn.commit()
+    cursor.close()
     return make_response(
-        jsonify(
-            mesagem = 'LISTA DE USUARIOS.',
-            pessoas = [p for p in pessoas if p["pessoas"] == nomeRecebidosDoFrontEnd]
-        )
-    )
+         jsonify(
+             mesagem = 'usuario cadastrado.',          
+         )
+     )
 
 # deletar um registro do banco de dados usando id com a função dropsql
 
-@app.route('/delete/<string:cpf>', methods=['DELETE'])
-def delete_pessoa(cpf):
-    indiceDaPessoa = next((index for (index, d) in enumerate(pessoas) if d["cpf"] == cpf), None)
-    pessoas.pop(indiceDaPessoa)
+@app.route('/delete/<string:usr_id>', methods=['DELETE'])
+def delete_pessoa(usr_id):
+    cursor = conn.cursor()
+    
+    query = "DELETE FROM tb_users WHERE usr_id = '{p2}';"
+    formatted_query = query.format(p2=usr_id)
+    cursor.execute(formatted_query)
+    conn.commit()
+    cursor.close()
+
     return  make_response(
-        jsonify("pessoa deletada! :)")
+    jsonify("pessoa deletada! :)")
     )
  
 app.run()
